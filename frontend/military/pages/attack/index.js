e5e8c526396a71813d9b090486bf51bacc7576f4
Abdullah Bin Faisal <paybacktanki@gmail.com>
2024-02-23 17:51:52 +0500

    Attack Page Done, except routing & DB Query

--- frontend/military/pages/attack/index.js
+++ frontend/military/pages/attack/index.js
@@ -13,6 +13,7 @@ function attackPage () {
     const [city, setCity] = useState("")
     const [lat, setLat] = useState(0)
     const [long, setLong] = useState(0)
+    const [invalid, setInvalid] = useState(false)
 
     const changeHandler = (e) => {
         setCity(e.target.value)
@@ -23,18 +24,29 @@ function attackPage () {
         const res = await fetch(`https://geocode.maps.co/search?q=${city}&api_key=65d8820377ee7965505431fjt1e9e47`);
         const response = await res.json();   
     
-        setLat(response[0].lat)
-        setLong(response[0].lon)   
+        if(response.length > 0){
+            setInvalid(false)
+            setLat(response[0].lat)
+            setLong(response[0].lon) 
+        }
+        else(
+            setInvalid(true)
+        ) 
         
     }
 
 
     if (lat === 0 && long ===0){
         return (
-            <>
-                <input type="text" onChange={(e) => {changeHandler(e)}} />
-                <button className='btn' onClick={latFinder}>Find</button>      
-            </>
+            <main className='flex flex-col min-h-screen bg-white items-center justify-center'>
+                <div>
+                    <h1 className='font-bold text-xl my-4 '>Attack a City.</h1>
+                    <input type="text" placeholder='Enter City Name' className='bg-gray-200 px-4 py-2 rounded-lg' onChange={(e) => {changeHandler(e)}} />
+                    <button className='px-4 mx-2 py-2 bg-primary-first text-white rounded-lg' onClick={latFinder}>Find</button>      
+                    {invalid ? <p className='text-red-400 my-2'> *City Not Found. Please check spellings. </p> : <p></p>}
+                </div>
+
+            </main>
         )
     } else
     {
