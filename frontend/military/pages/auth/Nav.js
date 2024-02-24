ded1bd51aaac892254d69266f578bcd5988e2883
Faaiz Umer <26100088@lums.edu.pk>
2024-02-24 14:50:30 +0500

    hghgf

--- frontend/military/pages/auth/Nav.js
+++ frontend/military/pages/auth/Nav.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import React from "react";
 export default function Nav() {
   return (
-    <nav className="flex justify-between px-10 py-2 lg:px-16 fixed min-w-full items-center shadow-md top-0">
+    <nav className="flex justify-between px-10 py-2 lg:px-16 min-w-full items-center shadow-md">
       <h1 className="font-semibold font-mono text-2xl text-primary-first">App</h1>
       <div className="flex items-center justify-center gap-4 text-primary-first font-mono">
         <span className="hover:text-primary-tertiary cursor-pointer">Log-out</span>
